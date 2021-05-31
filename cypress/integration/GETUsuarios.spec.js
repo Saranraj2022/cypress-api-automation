340cd6fddf2eeed59970eb564fa789e0767c81a9
Saranraj Prabhakaran <sprabhakaran@brightcove.com>
2021-05-31 12:39:54 +0530

    sample api tests

--- cypress/integration/GETUsuarios.spec.js
+++ cypress/integration/GETUsuarios.spec.js
@@ -1,16 +1,18 @@
-describe('Products api', () => {
+describe('Given the GET Users api', () => {
   context('GET /usuarios', () => {
-      it('should return a list with all usuarios', () => {
-          cy.request({
-              method: 'GET',
-              url: 'https://serverest.dev/usuarios'
-          }).should((response) => {
-              cy.log(JSON.stringify(response.body));
-              expect(response.status).to.eq(200);
-              expect(response.body.usuarios[0]).to.have.all.keys(
-                  'nome', 'email', 'password', 'administrador', '_id'
-                  );
-                });
-      });
-  });
-});
+    it('should return a list with all usuarios', () => {
+      cy.request({
+        method: 'GET',
+        url: '/usuarios'
+      }).should((response) => {
+        cy.log(JSON.stringify(response.body))
+        expect(response.status).to.eq(200)
+        cy.get(response.body.usuarios).each((user) => {
+          expect(user).to.have.all.keys(
+            'nome', 'email', 'password', 'administrador', '_id'
+          )
+        })
+      })
+    })
+  })
+})
